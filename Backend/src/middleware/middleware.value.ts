6e37eeb9d48b14def41c7b3fd374f37324362902
CRafaelS <carlos.rafael@engenharia.ufjf.br>
2022-11-18 02:56:09 -0300

    feat: middleware para value

---
+++ Backend/src/middleware/middleware.value.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from 'express';
+import HttpException from '../utils/httpException';
+
+export const isValidValue = (req: Request, res: Response, next: NextFunction) => {
+  const { value } = req.body;
+
+  if (!value || value === undefined) {
+    throw new HttpException(400, '"username" is required')
+  } 
+
+  if (typeof value !== 'number') {
+    throw new HttpException(422, '"username" must be a string')
+  }
+
+  if (value < 0) {
+    throw new HttpException(422, '"value" must be at greater than 0')
+  }
+  next();
+};
--- Backend/src/routers/index.ts
+++ Backend/src/routers/index.ts
@@ -5,6 +5,7 @@ import transactionController from '../controllers/transaction.controller';
 import filterController from '../controllers/filter.controller';
 import { isValidUsername, isValidPassword } from '../middleware/validateUser';
 import { authMiddleware } from '../middleware/middleware.auth';
+import { isValidValue } from '../middleware/middleware.value';
 
 const routers = Router();
 
@@ -12,9 +13,9 @@ routers.post('/client', isValidUsername, isValidPassword, clientController.newCl
 routers.get('/client', isValidUsername, isValidPassword, clientController.login);
 
 routers.get('/account', authMiddleware, accountController.getBalance);
-routers.post('/account', authMiddleware, accountController.deposit);
+routers.post('/account', authMiddleware, isValidValue, accountController.deposit);
 
-routers.post('/transaction', authMiddleware, transactionController);
+routers.post('/transaction', authMiddleware, isValidUsername, isValidValue, transactionController);
 
 routers.get('/filter/transactions', authMiddleware, filterController.getTransactions);
 routers.get('/filter/debit-or-credit', authMiddleware, filterController.filterCreditOrDebitTransaction);
